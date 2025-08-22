adddd5ef998141ad094c4f11db580789c3b6cd53
sousei-dev <dev@sousei-group.com>
2025-08-22 14:33:16 +0900

    보안처리로 인한 파일수정

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres.bbxqerucydvpjlohvcfh:zgkswkrnxo132!@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres"  # Supabase 접속정보 입력
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
